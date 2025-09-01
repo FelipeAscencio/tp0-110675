478f82997e2d4d795ae834b92f6ed9da91e448a4
Felipe Santino Ascencio <felipesantinoascencio@gmail.com>
2025-09-01 18:31:15 +0000

    Refactoring pequeño del script

--- mi-generador.py
+++ mi-generador.py
@@ -8,32 +8,8 @@ INDICE_CANT_CLIENTES = 2
 # Metadatos del stack.
 PROJECT_NAME            = "tp0"
 
-# Servicio Servidor.
-SERVER_NAME             = "server"
-SERVER_IMAGE            = "server:latest"
-SERVER_ENTRYPOINT       = "python3 /main.py"
-ENV_PYTHON_UNBUFFERED   = "PYTHONUNBUFFERED=1"
-SERVER_CONFIG           = "./server/config.ini:/config.ini"
-
-# Servicio Cliente.
-CLIENT_IMAGE            = "client:latest"
-CLIENT_PREFIX           = "client"
-CLIENT_ENTRYPOINT       = "/client"
-ENV_CLI_ID              = "CLI_ID"
-CLIENT_NOMBRE           = "Santiago Lionel"
-CLIENT_APELLIDO         = "Lorca"
-CLIENT_DOCUMENTO        = "30904465"
-CLIENT_NACIMIENTO       = "1999-03-17"
-CLIENT_NUMERO           = "7574"
-CLIENT_CONFIG           = "./client/config.yaml:/config.yaml"
-
-# Red.
-NETWORK_NAME            = "testing_net"
-NETWORK_DRIVER          = "default"
-NETWORK_SUBNET          = "172.25.125.0/24"
-
-def generate_file(filename, clients):
-    compose = f"""name: tp0
+def generar_archivo(nombre_archivo, clientes):
+    compose = f"""name: {PROJECT_NAME}
 services:
   server:
     container_name: server
@@ -41,14 +17,14 @@ services:
     entrypoint: python3 /main.py
     environment:
     - PYTHONUNBUFFERED=1
-    - CLIENTS={clients}
+    - CLIENTS={clientes}
     networks:
     - testing_net
     volumes:
       - ./server/config.ini:/config.ini
 """
     
-    for i in range(1, clients + 1):
+    for i in range(1, clientes + 1):
         compose += f"""
   client{i}:
     container_name: client{i}
@@ -74,13 +50,12 @@ networks:
         - subnet: 172.25.125.0/24
 """
     
-    with open(filename, "w") as f:
+    with open(nombre_archivo, "w") as f:
         f.write(compose)
     
-    print(f"Archivo {filename} generado con {clients} clientes.")
+    print(f"Archivo {nombre_archivo} generado con {clientes} clientes.")
 
 if __name__ == "__main__":
-    filename = sys.argv[1]
-    clients = int(sys.argv[2])
-    
-    generate_file(filename, clients)
+    nombre_archivo = sys.argv[INDICE_NOMBRE_ARCHIVO]
+    clientes = int(sys.argv[INDICE_CANT_CLIENTES])
+    generar_archivo(nombre_archivo, clientes)
