65ffe6109418b307c6c22a709265c1be41aa314a
Felipe Santino Ascencio <felipesantinoascencio@gmail.com>
2025-09-01 04:26:55 +0000

    Pulida de codigo del servidor

--- server/common/server.py
+++ server/common/server.py
@@ -1,22 +1,29 @@
+# Importación de los paquetes necesarios.
 import socket
 import logging
 import signal
 from common import utils
 
-
+# Clase que implementa el servidor.
 class Server:
+    # Constructor del servidor.
     def __init__(self, port, listen_backlog):
-        # Initialize server socket
         self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._server_socket.bind(('', port))
         self._server_socket.listen(listen_backlog)
         self.running = False
         self.client_sockets = []
 
+    # Loop principal del servidor (Modificación de código para manejar la señal pedida).
     def run(self):
+        """
+        Bucle de servidor ficticio.
+        El servidor acepta nuevas conexiones y establece comunicación con un cliente.
+        Una vez finalizada la comunicación con el cliente, el servidor vuelve a aceptar nuevas conexiones.
+        """
+
         self.running = True
         signal.signal(signal.SIGTERM, self.shutdown)
-
         while self.running:
             try:
                 client_sock = self.__accept_new_connection()
@@ -27,6 +34,7 @@ class Server:
                 if not self.running:
                     break
     
+    # Función que apaga el servidor de manera ordenada.
     def shutdown(self, signum=None, frame=None):
         self.running = False
         logging.info(f'action: shutdown | result: in_progress')
@@ -36,27 +44,40 @@ class Server:
             client_socket.close()
         logging.info(f'action: shutdown | result: success')
 
+    # Función que maneja la comunicación con un cliente específico.
     def __handle_client_connection(self, client_sock):
+        """
+        Lee el mensaje de un socket de cliente específico y lo cierra.
+        Si surge un problema en la comunicación con el cliente, el socket del
+        cliente también se cerrará.
+        """
+
         try:
-            bet_count = 0
+            contador_bet = 0
             while True:
-                bets, finish = utils.decode_bets(client_sock, bet_count)
-                if finish:
-                    utils.acknowledge_bets(client_sock, bet_count)
-                    logging.info(f'action: apuesta_recibida | result: success | cantidad: {bet_count}')
+                bets, terminado = utils.decode_bets(client_sock, contador_bet)
+                if terminado:
+                    utils.acknowledge_bets(client_sock, contador_bet)
+                    logging.info(f'action: apuesta_recibida | result: success | cantidad: {contador_bet}')
                     break
                 
                 utils.store_bets(bets)
-                bet_count += len(bets)
+                contador_bet += len(bets)
         except OSError as e:
             logging.error("action: receive_message | result: fail | error: {e}")
         finally:
             client_sock.close()
             self.client_sockets.remove(client_sock)
 
+    # Función que acepta nuevas conexiones de clientes.
     def __accept_new_connection(self):
-        # Connection arrived
+        """
+        Aceptar nuevas conexiones
+        La función se bloquea hasta que se establece la conexión con un cliente.
+        Después, se imprime y devuelve la conexión creada.
+        """
+
         logging.info('action: accept_connections | result: in_progress')
-        c, addr = self._server_socket.accept()
-        logging.info(f'action: accept_connections | result: success | ip: {addr[0]}')
+        c, direccion = self._server_socket.accept()
+        logging.info(f'action: accept_connections | result: success | ip: {direccion[0]}')
         return c
